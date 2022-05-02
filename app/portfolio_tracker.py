f470157ec8b79ce294ed4c07bf0f8c0b4baf9586
ishah2001 <30806568+ishah2001@users.noreply.github.com>
2022-05-02 17:43:52 -0400

    Added utils functions to portfolio-tracker.py

--- app/portfolio_tracker.py
+++ app/portfolio_tracker.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yfinance as yf
 import os
 from yahoofinancials import YahooFinancials
+from utils import get_current_price
 
 
 
@@ -16,11 +17,13 @@ for i in range(len(user_data)):
 	temp_ticker = user_data.iloc[i,0]
 	temp_share_count = user_data.iloc[i,1]
 	temp_date_purchased = user_data.iloc[i,2]
+	print(get_current_price(temp_ticker))
+	
 
 
 #def getReturnSincePurchase(ticker, purchase_date):
 
-print(tickers)
+
 
 
 
