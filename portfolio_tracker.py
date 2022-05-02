ef57dae849e4c93e3ad28f4b5ea1020d4a2e9f4e
ishah2001 <30806568+ishah2001@users.noreply.github.com>
2022-05-02 16:29:51 -0400

    for loop through data frame

--- portfolio_tracker.py
+++ portfolio_tracker.py
@@ -7,13 +7,19 @@ from yahoofinancials import YahooFinancials
 #Taking in user input
 
 user_data = pd.read_csv("C:/Users/17819/Desktop/UserInputs.csv")
+user_data.head()
 
+tickers = []
+for i in range(len(user_data)):
+	tickers.append(user_data.iloc[i,0])
 
 #def getReturnSincePurchase(ticker, purchase_date):
 
-stock = yf.Ticker("MSFT")
+print(tickers)
 
-purchase_date_price = stock
+
+
+
 
 
 
