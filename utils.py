0a2e1666a67d9e96acdc4919e235e8901db0cebd
dmorales07 <76737053+dmorales07@users.noreply.github.com>
2022-05-03 19:38:31 -0400

    Fpdf Info

--- utils.py
+++ utils.py
@@ -22,3 +22,26 @@ def get_Purchased_price(symbol):
 
 get_Purchased_price("AAPL")
 
+
+def output_df_to_pdf(pdf, df):
+    # A cell is a rectangular area, possibly framed, which contains some text
+    # Set the width and height of cell
+    table_cell_width = 25
+    table_cell_height = 6
+    # Select a font as Arial, bold, 8
+    pdf.set_font('Arial', 'B', 8)
+    
+    # Loop over to print column names
+    cols = df.columns
+    for col in cols:
+        pdf.cell(table_cell_width, table_cell_height, col, align='C', border=1)
+    # Line break
+    pdf.ln(table_cell_height)
+    # Select a font as Arial, regular, 10
+    pdf.set_font('Arial', '', 10)
+    # Loop over to print each data in the table
+    for row in df.itertuples():
+        for col in cols:
+            value = str(getattr(row, col))
+            pdf.cell(table_cell_width, table_cell_height, value, align='C', border=1)
+        pdf.ln(table_cell_height)
